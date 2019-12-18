25fee07c0f500dc29401cc90a4f556b84b78a809
SparkAngel <igor.gnatenko555@gmail.com>
2019-12-18 18:57:50 +0200

    Updates

---
+++ precache-manifest.2aa886ed991a3fd083d90c6d7bb8a3ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52d0f0d31395154858e6fd3598977104",
+    "url": "/test-game/index.html"
+  },
+  {
+    "revision": "2f7eb66f6a017a563b66",
+    "url": "/test-game/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "fe877177faf91717071c",
+    "url": "/test-game/static/css/main.b3e3c7e8.chunk.css"
+  },
+  {
+    "revision": "2f7eb66f6a017a563b66",
+    "url": "/test-game/static/js/2.5ded9811.chunk.js"
+  },
+  {
+    "revision": "fe877177faf91717071c",
+    "url": "/test-game/static/js/main.c314f036.chunk.js"
+  },
+  {
+    "revision": "d127df7bf1e02761c537",
+    "url": "/test-game/static/js/runtime~main.9a00ba65.js"
+  }
+]);
